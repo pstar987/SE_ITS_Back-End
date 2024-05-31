55352e7875a4b3c976810bb035b8fe894f96e525
pstar987 <pstar987@naver.com>
2024-05-31 17:36:56 +0900

    feat : 사용자 정보 업데이트 를 위한 dto 사용하지 않지만 임시 생성

--- src/main/java/com/se/its/domain/member/dto/request/MemberUpdateRequestDto.java
+++ src/main/java/com/se/its/domain/member/dto/request/MemberUpdateRequestDto.java
@@ -6,10 +6,15 @@ import lombok.Getter;
 @Getter
 public class MemberUpdateRequestDto {
 
-    @Email(message = "올바른 이메일 주소를 입력해주세요.")
-    private String email;
+    @NotBlank(message = "올바른 id를 입력해주세요.")
+    private String signId;
 
     @NotBlank(message = "PW는 공백이면 안 됩니다.")
     private String password;
+
+    @NotBlank(message = "PW는 공백이면 안 됩니다.")
+    private String passwordCheck;
+
+
 
 }
