f82d1a700e3dfbbffe5bde10c621bbb2d64aaaa0
lee0jae330 <xingxing2001@cau.ac.kr>
2024-06-02 05:38:20 +0900

    feat: LoginPage 매개변수로 SwingProjectController 추가

--- src/main/java/com/se/its/ITSApplication.java
+++ src/main/java/com/se/its/ITSApplication.java
@@ -3,6 +3,10 @@ package com.se.its;
 import com.se.its.domain.member.application.MemberService;
 import com.se.its.domain.member.domain.respository.MemberRepository;
 import com.se.its.domain.member.presentation.SwingMemberController;
+import com.se.its.domain.project.application.ProjectService;
+import com.se.its.domain.project.domain.repository.ProjectMemberRepository;
+import com.se.its.domain.project.domain.repository.ProjectRepository;
+import com.se.its.domain.project.presentation.SwingProjectController;
 import com.se.its.view.pages.LoginPage;
 import javax.swing.SwingUtilities;
 import org.springframework.boot.SpringApplication;
@@ -18,13 +22,16 @@ public class ITSApplication {
     public static void main(String[] args) {
         System.setProperty("java.awt.headless", "false");
         ConfigurableApplicationContext context = SpringApplication.run(ITSApplication.class, args);
-        //SpringApplication.run(ITSApplication.class, args);
         MemberRepository memberRepository = context.getBean(MemberRepository.class);
         MemberService memberService = new MemberService(memberRepository);
         SwingMemberController swingMemberController = new SwingMemberController(memberService);
 
+        ProjectRepository projectRepository = context.getBean(ProjectRepository.class);
+        ProjectMemberRepository projectMemberRepository = context.getBean(ProjectMemberRepository.class);
+        ProjectService projectService = new ProjectService(projectRepository,memberRepository,projectMemberRepository);
+        SwingProjectController swingProjectController = new SwingProjectController(projectService);
         SwingUtilities.invokeLater(() -> {
-            LoginPage loginPage = new LoginPage(swingMemberController);
+            LoginPage loginPage = new LoginPage(swingMemberController,swingProjectController);
             loginPage.setVisible(true);
         });
     }
