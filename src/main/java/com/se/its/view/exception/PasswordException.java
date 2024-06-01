fb573113bb77b38b4d82bb05de487dacb712f305
lee0jae330 <xingxing2001@cau.ac.kr>
2024-06-01 20:05:09 +0900

    refactor: 잘못된 상속 변경

--- src/main/java/com/se/its/view/exception/PasswordException.java
+++ src/main/java/com/se/its/view/exception/PasswordException.java
@@ -1,6 +1,6 @@
 package com.se.its.view.exception;
 
-abstract public class PasswordException extends IdException {
+abstract public class PasswordException extends IllegalArgumentException {
     public PasswordException(String message) {
         super(message);
     }
