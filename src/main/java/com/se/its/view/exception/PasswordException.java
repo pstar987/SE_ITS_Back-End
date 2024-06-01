7b5bc0c81fa985225646fb25f4a2d9d3e12caecf
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2024-06-01 17:15:29 +0900

    Revert "cross-plarform-controller와 swing 브랜치 merge"

--- src/main/java/com/se/its/view/exception/PasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.se.its.view.exception;
-
-abstract public class PasswordException extends IdException {
-    public PasswordException(String message) {
-        super(message);
-    }
-}
