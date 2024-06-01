f86fd95a667c7e9e83a8bc15a72be440de700474
Dustbox <sanggae4133@naver.com>
2024-06-01 19:52:33 +0900

    feat: Swing Project Controller 레이어 구현

--- src/main/java/com/se/its/domain/project/presentation/SwingProjectController.java
+++ src/main/java/com/se/its/domain/project/presentation/SwingProjectController.java
@@ -1,4 +1,44 @@
 package com.se.its.domain.project.presentation;
 
+import com.se.its.domain.project.application.ProjectService;
+import com.se.its.domain.project.dto.request.ProjectCreateRequestDto;
+import com.se.its.domain.project.dto.request.ProjectMemberAddRequestDto;
+import com.se.its.domain.project.dto.response.ProjectResponseDto;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 public class SwingProjectController {
+
+    private final ProjectService projectService;
+
+    public ProjectResponseDto createProject(
+            Long id,
+            ProjectCreateRequestDto projectCreateRequestDto
+    ) {
+        return projectService.createProject(id, projectCreateRequestDto);
+    }
+
+    public List<ProjectResponseDto> getAllProject(
+            Long id
+    ) {
+        return projectService.getAllProject(id);
+    }
+
+    public ProjectResponseDto getProject(
+            Long id,
+            Long projectId
+    ) {
+        return projectService.getProjectById(id, projectId);
+    }
+
+    public ProjectResponseDto addMember(
+            Long id,
+            Long projectId,
+            ProjectMemberAddRequestDto projectMemberAddRequestDto
+    ){
+        return projectService.addMember(id, projectId, projectMemberAddRequestDto);
+    }
+
 }
