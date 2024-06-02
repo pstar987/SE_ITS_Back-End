7548c06e4f442c80cca9f1b6f71f826eed9a428b
leeyeongjae <xingxing2001@cau.ac.kr>
2024-06-03 00:03:44 +0900

    feat: 개발자가 자신에게 할당된 이슈 조회 페이지 추가

---
+++ src/main/java/com/se/its/view/pages/DevIssueBrowsePage.java
@@ -0,0 +1,15 @@
+package com.se.its.view.pages;
+
+import com.se.its.domain.issue.presentation.SwingIssueController;
+import com.se.its.domain.project.dto.response.ProjectResponseDto;
+import javax.swing.*;
+
+public class DevIssueBrowsePage extends JFrame {
+    private SwingIssueController swingIssueController;
+    private ProjectResponseDto currentProject;
+    private Long userId;
+
+    public DevIssueBrowsePage() {
+
+    }
+}
