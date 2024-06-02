6ecb5ee29f07e4fea320aae7c7a35c24a82e6a1a
leeyeongjae <xingxing2001@cau.ac.kr>
2024-06-02 17:41:03 +0900

    feat: login페이지에 의존성 추가

--- src/main/java/com/se/its/ITSApplication.java
+++ src/main/java/com/se/its/ITSApplication.java
@@ -1,6 +1,8 @@
 package com.se.its;
 
+import com.se.its.domain.issue.application.IssueService;
 import com.se.its.domain.issue.domain.repository.IssueRepository;
+import com.se.its.domain.issue.presentation.SwingIssueController;
 import com.se.its.domain.member.application.MemberService;
 import com.se.its.domain.member.domain.respository.MemberRepository;
 import com.se.its.domain.member.presentation.SwingMemberController;
@@ -29,19 +31,21 @@ public class ITSApplication {
         MemberRepository memberRepository = context.getBean(MemberRepository.class);
         DtoConverter dtoConverter = context.getBean(DtoConverter.class);
         EntityValidator entityValidator = context.getBean(EntityValidator.class);
-        MemberService memberService = new MemberService(memberRepository, dtoConverter,entityValidator);
-        SwingMemberController swingMemberController = new SwingMemberController(memberService);
 
         ProjectRepository projectRepository = context.getBean(ProjectRepository.class);
         ProjectMemberRepository projectMemberRepository = context.getBean(ProjectMemberRepository.class);
         IssueRepository issueRepository = context.getBean(IssueRepository.class);
 
+        MemberService memberService = new MemberService(memberRepository, dtoConverter,entityValidator);
         ProjectService projectService = new ProjectService(projectRepository, projectMemberRepository,issueRepository, dtoConverter, entityValidator);
+        IssueService issueService = new IssueService(issueRepository, dtoConverter, entityValidator);
 
+        SwingMemberController swingMemberController = new SwingMemberController(memberService);
         SwingProjectController swingProjectController = new SwingProjectController(projectService);
+        SwingIssueController swingIssueController = new SwingIssueController(issueService);
 
         SwingUtilities.invokeLater(() -> {
-            LoginPage loginPage = new LoginPage(swingMemberController, swingProjectController);
+            LoginPage loginPage = new LoginPage(swingMemberController, swingProjectController, swingIssueController);
             loginPage.setVisible(true);
         });
     }
