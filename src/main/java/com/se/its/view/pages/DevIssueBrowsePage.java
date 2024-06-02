5a7f5442a1de26eb0e19e1ad2ccfea005abea543
leeyeongjae <xingxing2001@cau.ac.kr>
2024-06-03 05:20:05 +0900

    feat: DEV가 자신에게 할당된 issue들만 조회하는 기능 추가

--- src/main/java/com/se/its/view/pages/DevIssueBrowsePage.java
+++ src/main/java/com/se/its/view/pages/DevIssueBrowsePage.java
@@ -1,15 +1,134 @@
 package com.se.its.view.pages;
 
+import com.se.its.domain.issue.dto.response.IssueResponseDto;
 import com.se.its.domain.issue.presentation.SwingIssueController;
 import com.se.its.domain.project.dto.response.ProjectResponseDto;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import javax.swing.*;
 
 public class DevIssueBrowsePage extends JFrame {
     private SwingIssueController swingIssueController;
     private ProjectResponseDto currentProject;
-    private Long userId;
+    private final Long userId;
 
-    public DevIssueBrowsePage() {
+    private List<IssueResponseDto> issueResponseDtos;
+    private JList<IssueResponseDto> issueResponseDtoJList;
 
+    public DevIssueBrowsePage(SwingIssueController swingIssueController, ProjectResponseDto currentProject,
+                              Long userId) {
+        this.swingIssueController = swingIssueController;
+        this.currentProject = currentProject;
+        this.userId = userId;
+
+        setTitle("할당된 이슈 조회");
+        setSize(400, 500);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        initComponents();
+    }
+
+    private void initComponents() {
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        JLabel titleLabel = new JLabel("할당된 이슈 조회");
+        titleLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
+        add(titleLabel, gbc);
+
+        gbc.gridy = 1;
+        JLabel subTitle = new JLabel("이슈 리스트");
+        add(subTitle, gbc);
+
+        issueResponseDtoJList = new JList<>(new DefaultListModel<>());
+        initAssignIssueList(currentProject, swingIssueController, userId);
+        issueResponseDtoJList.setCellRenderer(new IssueListRender());
+
+        JScrollPane scrollPane = new JScrollPane(issueResponseDtoJList);
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        add(scrollPane, gbc);
+
+    }
+
+    private class IssueListRender extends JPanel implements ListCellRenderer<IssueResponseDto> {
+        private JLabel issueName;
+        private JLabel issuePriority;
+        private JLabel issueStatus;
+        private JLabel issueAssignee;
+        private JLabel issueReportedDate;
+        private DateTimeFormatter formatter;
+
+        public IssueListRender() {
+            setLayout(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.insets = new Insets(5, 5, 5, 5);
+
+            issueName = new JLabel();
+            issuePriority = new JLabel();
+            issueStatus = new JLabel();
+            issueAssignee = new JLabel();
+            issueReportedDate = new JLabel();
+            formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd:HH:mm");
+
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.anchor = GridBagConstraints.WEST;
+            add(issueName, gbc);
+
+            gbc.gridx = 1;
+            add(issueStatus, gbc);
+
+            gbc.gridx = 2;
+            add(issuePriority, gbc);
+
+            gbc.gridx = 3;
+            add(issueAssignee, gbc);
+
+            gbc.gridx = 4;
+            add(issueReportedDate, gbc);
+        }
+
+        @Override
+        public Component getListCellRendererComponent(JList<? extends IssueResponseDto> list, IssueResponseDto value,
+                                                      int index, boolean isSelected, boolean cellHasFocus) {
+            issueName.setText("[" + value.getTitle() + "]");
+            issueStatus.setText("상태: " + value.getStatus());
+            issuePriority.setText("우선순위: " + value.getPriority());
+            issuePriority.setText("담당자: " + (value.getAssignee() == null ? "지정 안됨" : value.getAssignee().getName()));
+            issueReportedDate.setText("일시: " + value.getReportedDate().format(formatter));
+            if (isSelected) {
+                setBackground(list.getSelectionBackground());
+                setForeground(list.getSelectionForeground());
+            } else {
+                setBackground(list.getBackground());
+                setForeground(list.getForeground());
+            }
+            return this;
+        }
+    }
+
+    private void initAssignIssueList(ProjectResponseDto currentProject, SwingIssueController swingIssueController,
+                                     Long userId) {
+        issueResponseDtos = swingIssueController.getDevIssues(userId, currentProject.getId());
+        DefaultListModel<IssueResponseDto> listModel = (DefaultListModel<IssueResponseDto>) issueResponseDtoJList.getModel();
+        for (IssueResponseDto issueResponseDto : issueResponseDtos) {
+            listModel.addElement(issueResponseDto);
+        }
     }
 }
